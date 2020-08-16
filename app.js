b03df17f09d89e865ee5e4513b7b95bdad0d2e4c
Adnan musil <adnan.museel123@gmail.com>
2020-08-16 22:42:30 +0500

    chp 1 2 deploy

---
+++ app.js
@@ -0,0 +1,19 @@
+//class 1 extensions
+// class2
+
+// console.time("Your code took");
+// console.log("hello world");
+// console.log(10+5);
+// console.log(true)
+// console.log([1,2,3,4,5]);
+// console.log({name:"abc",roll:123});
+// console.table({name:"abc",roll:123});
+// console.warn("this is a warning");
+// // console.clear();
+// console.timeEnd("your code took")
+// // console.assert(566 < 189,"age > 189 is not possible");
+// console.error("this is an error");
+
+
+
+
