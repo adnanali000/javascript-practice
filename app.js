f670c2828eb0db5d5a2f38b1fcb5de23a615148c
Adnan musil <adnan.museel123@gmail.com>
2020-08-16 23:01:55 +0500

    chp 3 code deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //class 1 extensions
-// class2
+// class2 console
 
 // console.time("Your code took");
 // console.log("hello world");
@@ -15,5 +15,53 @@
 // console.error("this is an error");
 
 
+//class 3 let,const,var
 
+/*
+rules for assigning variable
+
+1.Cannot start with number
+2.can start with letter , _ , $
+3.are case sensitive
+
+*/
+
+// var name = "hello world";
+// var first_name = "hello";
+// var last_$name = "world";
+// var fullname1 = "hello world";
+
+//In modern javascript mostly developers used let and const
+
+//const use only one time and will never replace by any variable
+
+// const ownerName = "jeff bezof";
+// console.log(ownerName);
+
+//let has block level scope
+// {
+//     let city = "rome";
+//     city = "islamabad"
+//     console.log(city); //islamabad
+// }
+// console.log(city);//islamabad
+
+//const arr value can update but not assign again with same variable name 
+//i.e const arr = [1,2,3]
+//arr = [3,4,5] //error while using const
+
+// const arr = [1,2,3,4,5];
+// arr.push(10);
+// console.log(arr);
+
+
+//cases in javascript
+/*
+1.camelCase //most usable
+2.kebab-case
+3.snake_case
+4.PascalCase
+*/
+
+// class 4
 
