783242847dd1a42a2af1d891accb253ca2572446
Adnan musil <adnan.museel123@gmail.com>
2020-09-10 21:48:58 +0500

    regular expression literal code deploy

--- app2.js
+++ app2.js
@@ -375,7 +375,7 @@ console.log('last line of this js file');
         //class 44 error handling
 
 //pretend this is coming from a server as response
-
+/*
 let a = 'adnan bhai';
 a = undefined
 if(a != undefined){
@@ -397,12 +397,262 @@ try {
 }finally {
     console.log('finally block run')
 }
+*/
+
+ 
+           //Excercise 5 solution
+
+           /*
+let myJson = `{
+    "word": "example",
+    "results": [
+      {
+        "definition": "a representative form or pattern",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "model"
+        ],
+        "typeOf": [
+          "representation",
+          "internal representation",
+          "mental representation"
+        ],
+        "hasTypes": [
+          "prefiguration",
+          "archetype",
+          "epitome",
+          "guide",
+          "holotype",
+          "image",
+          "loadstar",
+          "lodestar",
+          "microcosm",
+          "original",
+          "paradigm",
+          "pilot",
+          "prototype",
+          "template",
+          "templet",
+          "type specimen"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "I profited from his example"
+        ]
+      },
+      {
+        "definition": "something to be imitated",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exemplar",
+          "good example",
+          "model"
+        ],
+        "typeOf": [
+          "ideal"
+        ],
+        "hasTypes": [
+          "pacemaker",
+          "pattern",
+          "beauty",
+          "prodigy",
+          "beaut",
+          "pacesetter"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ]
+      },
+      {
+        "definition": "an occurrence of something",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "case",
+          "instance"
+        ],
+        "typeOf": [
+          "happening",
+          "natural event",
+          "occurrence",
+          "occurrent"
+        ],
+        "hasTypes": [
+          "clip",
+          "mortification",
+          "piece",
+          "time",
+          "humiliation",
+          "bit"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "but there is always the famous example of the Smiths"
+        ]
+      },
+      {
+        "definition": "an item of information that is typical of a class or group",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "illustration",
+          "instance",
+          "representative"
+        ],
+        "typeOf": [
+          "information"
+        ],
+        "hasTypes": [
+          "excuse",
+          "apology",
+          "specimen",
+          "case in point",
+          "sample",
+          "exception",
+          "quintessence",
+          "precedent"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ],
+        "examples": [
+          "this patient provides a typical example of the syndrome",
+          "there is an example on page 10"
+        ]
+      },
+      {
+        "definition": "punishment intended as a warning to others",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "deterrent example",
+          "lesson",
+          "object lesson"
+        ],
+        "typeOf": [
+          "monition",
+          "admonition",
+          "word of advice",
+          "warning"
+        ],
+        "derivation": [
+          "exemplary"
+        ],
+        "examples": [
+          "they decided to make an example of him"
+        ]
+      },
+      {
+        "definition": "a task performed or problem solved in order to develop skill or understanding",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exercise"
+        ],
+        "typeOf": [
+          "lesson"
+        ],
+        "examples": [
+          "you must work the examples at the end of each chapter in the textbook"
+        ]
+      }
+    ],
+    "syllables": {
+      "count": 3,
+      "list": [
+        "ex",
+        "am",
+        "ple"
+      ]
+    },
+    "pronunciation": {
+      "all": "ɪɡ'zæmpəl"
+    },
+    "frequency": 4.67
+  }`
+
+
+  let myObj = JSON.parse(myJson);
+  console.log('The object is : ',myObj);
+  console.log('The result in the object : ',myObj['results']);
+
+let meanings = document.getElementById('meanings');
+meanings.addEventListener('click',()=>{
+    console.log('someone clicked meanings')
+
+    populate();
+})
+
+function populate() {
+    let results = myObj['results'];
+    let html = "";
+    results.forEach(element => {
+        html += `<li>One of the definitions of example is ${element.definition}</li>`;
+    });
+    let defs = document.getElementById('defs');
+    defs.innerHTML = html;
+}
+*/
+
+        //  class 46 Regular expression
+
+//let reg = /hello/; // regular expression literal
+//reg = /hello/g; //global expression
+//reg = /hello/i; //i mean case insensitive
+
+//console.log(reg);
+//console.log(reg.source);
+
+//let str = "Welome to hello world and hello world";
+
+//function to match the value of regular expression
+//1. exec() : this function return the array if value found and null if value not found
+
+//let result = reg.exec(str);
+//console.log(result)
+//console.log(result.input)
+//console.log(result.index)
+
+
+// result = reg.exec(str);
+// console.log(result)
+// result = reg.exec(str);
+// console.log(result)
+
+//2.test() return true if value found or false if not found
+
+//let result2 = reg.test(str);
+//console.log(result2);
+
+//3 match() return an array of result or null
+//let result3 = reg.match(str); //this is wrong way
+
+//let result3 = str.match(reg); //this is right way
+
+//console.log(result3);
+
+
+//4 search() return index of first if match else -1
+
+//let result4 = str.search(reg);
+//console.log(result4);
+
+
+
+//5 replace() return new string with all replacement if global flag given otherwise first match will be replaced
+
+//let result5 = str.replace(reg,'hey');
+//console.log(result5);
 
 
 
 
 
 
+
+  
 
 
 
